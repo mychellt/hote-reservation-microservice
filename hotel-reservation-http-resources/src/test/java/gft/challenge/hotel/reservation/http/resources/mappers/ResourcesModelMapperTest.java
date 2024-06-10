23bb92aaf8997527b3a593c06b1465cd599ca6e5
Mychell Teixeira <mychellt@gmail.com>
2024-06-10 01:38:25 -0300

    feat: unit test for mapper.

--- hotel-reservation-http-resources/src/test/java/gft/challenge/hotel/reservation/http/resources/mappers/ResourcesModelMapperTest.java
+++ hotel-reservation-http-resources/src/test/java/gft/challenge/hotel/reservation/http/resources/mappers/ResourcesModelMapperTest.java
@@ -1,4 +1,38 @@
 package gft.challenge.hotel.reservation.http.resources.mappers;
 
-public class ResourcesModelMapperTest {
+import gft.challenge.hotel.reservation.domain.Hotel;
+import gft.challenge.hotel.reservation.http.resources.dto.FetchHotelDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyOrNullString;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ResourcesModelMapperTest {
+  @Test
+  @DisplayName(value = "Verify mapping from domain model to DTO")
+  void from_test() {
+    ResourcesModelMapper mapper = Mappers.getMapper(ResourcesModelMapper.class);
+    FetchHotelDTO dto = mapper.from(Hotel.builder()
+        .name("Fake Name")
+        .id(UUID.randomUUID())
+        .email("fake@dev.com")
+        .phone("Fake Phone")
+        .availableRooms(1)
+        .numberOfRooms(1)
+        .build());
+
+    assertNotNull(dto);
+    assertNotNull(dto.getNumberOfRooms());
+    assertNotNull(dto.getAvailableRooms());
+    assertThat(dto.getPhone(), not(emptyOrNullString()));
+    assertThat(dto.getName(), not(emptyOrNullString()));
+    assertThat(dto.getEmail(), not(emptyOrNullString()));
+
+  }
 }
