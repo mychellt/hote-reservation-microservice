90043f5a519a79fc17236df2eb596f7d097c9183
Mychell Teixeira <mychellt@gmail.com>
2024-06-10 01:41:48 -0300

    feat: Unit test for processors.

---
+++ hotel-reservation-processor/src/test/java/gft/challenge/hotel/reservation/processor/CreateHotelProcessorAdapterTest.java
@@ -0,0 +1,9 @@
+package gft.challenge.hotel.reservation.processor;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CreateHotelProcessorAdapterTest {
+
+}
---
+++ hotel-reservation-processor/src/test/java/gft/challenge/hotel/reservation/processor/ReservationHotelAdapterTest.java
@@ -0,0 +1,9 @@
+package gft.challenge.hotel.reservation.processor;
+
+import org.junit.jupiter.api.extension.ExecutionCondition;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(ExecutionCondition.class)
+class ReservationHotelAdapterTest {
+
+}
